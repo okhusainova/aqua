583c3683a8a68265db9030a7148e4ee85c633056
okhusainova <olga.khusainov@gmail.com>
2016-09-05 10:21:21 +0300

    Add map

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,15 @@
-// Slider Slick
-	$(document).ready(function(){
-  $('.slider').slick({autoplay: true, arrows: 'false', dots: true});
-});
+function initMap() {
+        var myLatLng = {lat: 44.601526, lng: 42.270338};
+
+        var map = new google.maps.Map(document.getElementById('map'), {
+          center: myLatLng,
+          scrollwheel: false,
+          zoom: 9
+        });
+
+        var marker = new google.maps.Marker({
+          map: map,
+          position: myLatLng,
+          title: 'Hello World!'
+        });
+  }
