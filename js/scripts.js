28e46a5916504899fadb7280d468b612d6cd6426
okhusainova <olga.khusainov@gmail.com>
2016-09-06 23:06:29 +0300

    Add map.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,4 @@
-function initMap() {
-        var myLatLng = {lat: 44.601526, lng: 42.270338};
 
-        var map = new google.maps.Map(document.getElementById('map'), {
-          center: myLatLng,
-          scrollwheel: false,
-          zoom: 9
-        });
-
-        var marker = new google.maps.Marker({
-          map: map,
-          position: myLatLng,
-          title: 'Hello World!'
-        });
-  }
 
   $(document).ready(function(){
 		  $('.head-slider').slick({autoplay: true, arrows: 'false', dots: true});
