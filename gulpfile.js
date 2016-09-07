b67ea7630b1da59dd0ce3051ae62617c66ae6ba9
okhusainova <olga.khusainov@gmail.com>
2016-09-07 20:16:54 +0300

    pre-final version

--- gulpfile.js
+++ gulpfile.js
@@ -2,30 +2,29 @@ var gulp = require('gulp');
 var less = require('gulp-less');
 var autoprefixer = require('gulp-autoprefixer');
 var browserSync = require('browser-sync').create();
- browserSync.init({
-     server: "./"
- });
- browserSync.stream();
- gulp.watch("less/*.less", ['styles']);
- gulp.watch("*.html").on('change', browserSync.reload);
+browserSync.init({
+    server: "./"
+});
+browserSync.stream();
+gulp.watch("less/*.less", ['styles']);
+gulp.watch("*.html").on('change', browserSync.reload);
 
 gulp.task('default', function() {
-  // place code for your default task here
+    // place code for your default task here
 });
 
 gulp.task('styles', function() {
-	gulp.src('less/**/*.less')
-	.pipe(less())
-	.pipe(autoprefixer({
+    gulp.src('less/**/*.less')
+        .pipe(less())
+        .pipe(autoprefixer({
             browsers: ['last 2 versions']
-        }
-        ))
-	.pipe(gulp.dest('./css'))
-	.pipe(browserSync.stream());
+        }))
+        .pipe(gulp.dest('./css'))
+        .pipe(browserSync.stream());
 });
 
 var jquery = require('gulp-jquery');
-gulp.task('jquery', function () {
+gulp.task('jquery', function() {
     return gulp.src('./node_modules/jquery-custom/jquery.1/src')
         .pipe(jquery({
             flags: ['-deprecated', '-event/alias', '-ajax/script', '-ajax/jsonp', '-exports/global']
